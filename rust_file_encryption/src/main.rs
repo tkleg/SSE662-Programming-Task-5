6e4e93a59375df19fea5836eba4c85f2948c02ba
tkleg <thkleger@gmail.com>
2026-03-16 17:01:50 -0400

    creation of file is working

--- rust_file_encryption/src/main.rs
+++ rust_file_encryption/src/main.rs
@@ -1,16 +1,62 @@
 use std::io;
 use std::fs::File;
+use std::str;
 
 fn main() {
-    let file = getInputFile();
-    match file {
+    let inputFileResult = getInputFile();
+    let inputFile : File;
+    match inputFileResult {
         Ok(f) => {
-            println!("File opened successfully: {:?}", f);
+            inputFile = f;
+            println!("File opened successfully: {:?}", inputFile);
         }
         Err(e) => {
-            println!("Error opening file: {}", e);
+            println!("Error opening file: {}. Exiting program.", e);
+            return;
         }
     }
+
+    let cipher : str::Chars = "0xexcellentParade".chars();
+
+    let outputFileResult = getOutputFile();
+    let outputFile : File;
+
+    match outputFileResult {
+        Ok(f) => {
+            outputFile = f;
+            println!("File created successfully: {:?}", outputFile);
+        }
+        Err(e) => {
+            println!("Error creating file: {}. Exiting program.", e);
+            return;
+        }
+    }
+}
+
+fn getOutputFile() -> Result<File, io::Error> {
+    let mut outputFilePath = String::from("data/encrypted/");
+    println!("Enter path for output file. Note that paths start from data/encrypted/:");
+    let outputFilePathResult : io::Result<usize> = io::stdin().read_line(&mut outputFilePath);
+    match outputFilePathResult {
+        Ok(_) => {
+            println!("Output File Path: {}", outputFilePath.trim());
+        }
+        Err(e) => {
+            println!("Cannot read input: {}", e);
+            return Err(e);
+        }
+    }
+
+    let f2 : File = match File::create(outputFilePath.trim()) {
+        Ok(file) => {
+            println!("getOutputFile created file with path {} successfully", outputFilePath.trim());
+            return Ok(file);
+        }
+        Err(e) => {
+            println!("getOutputFile cannot create file with path {}: {}", outputFilePath.trim(), e);
+            return Err(e);
+        }
+    };
 }
 
 fn getInputFile() -> Result<File, io::Error> {
