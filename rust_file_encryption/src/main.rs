c9e4ca9adbe933d4f4cda3823c1dff11ea6864f8
tkleg <thkleger@gmail.com>
2026-03-16 16:42:12 -0400

    file retrievel sent to file

--- rust_file_encryption/src/main.rs
+++ rust_file_encryption/src/main.rs
@@ -2,6 +2,18 @@ use std::io;
 use std::fs::File;
 
 fn main() {
+    let file = getInputFile();
+    match file {
+        Ok(f) => {
+            println!("File opened successfully: {:?}", f);
+        }
+        Err(e) => {
+            println!("Error opening file: {}", e);
+        }
+    }
+}
+
+fn getInputFile() -> Result<File, io::Error> {
     let mut inputFilePath = String::from("data/raw/");
     println!("Enter path for file one. Note that paths start from data/raw/:");
     let inputFilePathResult : io::Result<usize> = io::stdin().read_line(&mut inputFilePath);
@@ -11,18 +23,18 @@ fn main() {
         }
         Err(e) => {
             println!("Cannot read input: {}", e);
-            return;
+            return Err(e);
         }
     }
 
     let f1 : File = match File::open(inputFilePath.trim()) {
         Ok(file) => {
-            println!("File opened successfully");
-            file
+            println!("getInputFile opened file with path {} successfully", inputFilePath.trim());
+            return Ok(file);
         }
         Err(e) => {
-            println!("Cannot open file: {}. Path {}", e, inputFilePath.trim());
-            return;
+            println!("getInputFile cannot open file with path {}: {}", inputFilePath.trim(), e);
+            return Err(e);
         }
     };
 }
